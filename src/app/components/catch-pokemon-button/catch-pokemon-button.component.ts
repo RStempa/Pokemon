4e9c3da1da4e5da807b58e7104722e66cd8446a5
Mattias Smedman <mattias.smedman@gmail.com>
2023-01-31 14:19:39 +0100

    Add catch functionality & sessionstorage
    feat: User can now catch pokemon and they get added to the users list in the API. Additionally it now saves the pokemon to the sessionstorage and loads from it instead of the API when possible.
    
    WIP #1

--- src/app/components/catch-pokemon-button/catch-pokemon-button.component.ts
+++ src/app/components/catch-pokemon-button/catch-pokemon-button.component.ts
@@ -1,5 +1,7 @@
+import { HttpErrorResponse } from '@angular/common/http';
 import { Component, Input } from '@angular/core';
-
+import { PokemonService } from 'src/app/services/pokemon.service';
+import { User } from 'src/app/models/user.model';
 @Component({
   selector: 'app-catch-pokemon-button',
   templateUrl: './catch-pokemon-button.component.html',
@@ -8,8 +10,17 @@ import { Component, Input } from '@angular/core';
 export class CatchPokemonButtonComponent {
 
   @Input() pokemonName: string = "";
-
+  constructor(private readonly pokemonService: PokemonService) {}
   onCatchClick(): void{
     console.log(this.pokemonName);
+    this.pokemonService.addToCollection(this.pokemonName).subscribe({
+      next: (response: User) => {
+        console.log("NEXT", response);
+      },
+      error: (error: HttpErrorResponse) => 
+      {
+        console.log(error.message);
+      }
+    });
   }
 }
