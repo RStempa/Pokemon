556c10a65a12acaac65f780111f1801c835aa152
Robin Stempa <stemparobin@gmail.com>
2023-02-01 10:55:18 +0100

    removed environments folder

--- src/environments/environment.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-    production: false,
-    apiUsers: "https://gratis-precious-ringer.glitch.me/trainers",
-    apiPokemon: "https://pokeapi.co/api/v2/pokemon",
-    ApiKey: "e7j6Zybdx7T2yUJ9tB5Jc9XWvfYm4bADfpruDpYvPF5uQ4WFANyQPSEWX28WeXnP"
-}
